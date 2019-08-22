d396e8858ee9f964ba9a7b7c93303c6cf8eca045
yanch <ongyc1999@gmail.com>
2019-08-22 18:46:54 +0800

    functions: add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
+        ArrayList<String> text = new ArrayList<>();
         Scanner input = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -11,7 +13,15 @@ public class Duke {
         System.out.println("    Hello! I'm Duke!\n    What can I do for you?");
         String currentInput = input.nextLine();
         while (!currentInput.equals("bye")) {
-            System.out.println("    " + currentInput);
+            if (currentInput.equals("list")) {
+                int i = 1;
+                for (String s : text) {
+                    System.out.println("    " + i++ + ". " + s);
+                }
+            } else {
+                System.out.println("    added: " + currentInput);
+                text.add(currentInput);
+            }
             currentInput = input.nextLine();
         }
         System.out.println("    Bye. Hope to see you again soon!");
