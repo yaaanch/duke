15a81040e7d45a241416d68ecd99e6c837f43a38
yanch <ongyc1999@gmail.com>
2019-08-22 18:39:01 +0800

    Skeletal version of Duke.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("    Hello! I'm Duke!\n    What can I do for you?");
+        String currentInput = input.nextLine();
+        while (!currentInput.equals("bye")) {
+            System.out.println("    " + currentInput);
+            currentInput = input.nextLine();
+        }
+        System.out.println("    Bye. Hope to see you again soon!");
     }
 }
