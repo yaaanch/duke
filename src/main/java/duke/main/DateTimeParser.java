57857b1037c7411a7e07422bba644e0907033a0c
yanch <8232205+yaaanch@users.noreply.github.com>
2019-09-16 21:31:52 +0800

    Added some new stuff, not working.

--- src/main/java/duke/main/DateTimeParser.java
+++ src/main/java/duke/main/DateTimeParser.java
@@ -1,24 +1,34 @@
 package duke.main;
 
+//import com.joestelmach.natty.DateGroup;
+//import com.joestelmach.natty.DateGroup;
 import duke.error.IncorrectDateTimeError;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+//import java.util.List;
+//import com.joestelmach.natty.Parser;
 
 public class DateTimeParser {
     public DateTimeParser() {
     }
 
     public static Date parse(String s) throws IncorrectDateTimeError {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HHmm");
-        try {
-            Date date = formatter.parse(s);
-            return date;
-        } catch (ParseException e) {
-            throw new IncorrectDateTimeError();
-        }
+//        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HHmm");
+//        try {
+//            Date date = formatter.parse(s);
+//            return date;
+//        } catch (ParseException e) {
+//            throw new IncorrectDateTimeError();
+//        }
+        com.joestelmach.natty.Parser parser = new com.joestelmach.natty.Parser();
+        List<com.joestelmach.natty.DateGroup> groups = parser.parse("the day before next thursday");
+        return groups.get(0).getDates().get(0);
     }
+
+
 
     public static String format(Date d) {
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HHmm");
